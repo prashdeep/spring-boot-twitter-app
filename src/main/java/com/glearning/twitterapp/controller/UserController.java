3fbcf799019fcb257d9faa995775e30398b56dad
testuser+1-at-501188795578 <pradeep.kumar44@gmail.com>
2019-09-01 13:05:23 +0530

    committing the swagger documentation

--- src/main/java/com/glearning/twitterapp/controller/UserController.java
+++ src/main/java/com/glearning/twitterapp/controller/UserController.java
@@ -2,6 +2,10 @@ package com.glearning.twitterapp.controller;
 
 import com.glearning.twitterapp.model.User;
 import com.glearning.twitterapp.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,17 +20,22 @@ import static org.springframework.http.MediaType.*;
 @RequestMapping("/api/v1/users/")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping(value = "/")
+    @ApiOperation(value = "Fetch all users", notes = "API to fetch all the users")
     public List<User> getAllUsers() {
+        logger.info("-- Came inside the getALlUsers method --");
         return this.userService.listAll();
     }
 
     @GetMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public User getUserById(@PathVariable("id")long id){
+    @ApiOperation(value = "Fetch users details for the given id", notes = "API to fetch the user details")
+    public User getUserById(@ApiParam(value = "id", example = "34", required = true) @PathVariable("id")long id){
         return this.userService.findById(id);
     }
 
@@ -35,6 +44,11 @@ public class UserController {
     public User saveUser(@RequestBody User user){
         User saveUser = this.userService.saveUser(user);
         return saveUser;
+    }
+
+    @GetMapping("/{id}/followers")
+    public List<User> getFollowersByUserId(@PathVariable("id") long id){
+        return this.userService.listAll();
     }
 
 
